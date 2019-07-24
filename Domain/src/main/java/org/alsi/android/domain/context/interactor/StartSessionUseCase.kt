a563ffcaa4cc043c8de577950d69e77e57f31edc
Alexander S. Sidorov <alesidos@gmail.com>
2019-07-24 14:48:00 +0300

    RX Java dependency added; deprecated "toComplete" fixed

--- Domain/src/main/java/org/alsi/android/domain/context/interactor/StartSessionUseCase.kt
+++ Domain/src/main/java/org/alsi/android/domain/context/interactor/StartSessionUseCase.kt
@@ -1,26 +1,19 @@
 package org.alsi.android.domain.context.interactor
 
 import io.reactivex.Completable
-import org.alsi.android.domain.context.model.PresentationManager
-import org.alsi.android.domain.implementation.interactor.CompletableUseCase
 import org.alsi.android.domain.implementation.executor.PostExecutionThread
+import org.alsi.android.domain.implementation.interactor.CompletableUseCase
+import org.alsi.android.domain.streaming.model.ServiceProvider
 import javax.inject.Inject
 
-open class StartSessionUseCase @Inject constructor(
-        private val presentationManager: PresentationManager,
+open class  StartSessionUseCase @Inject constructor(
+        private val provider: ServiceProvider,
         executionThread: PostExecutionThread)
     : CompletableUseCase<StartSessionUseCase.Params>(executionThread)
 {
     override fun buildUseCaseCompletable(params: Params?): Completable {
         params?: throw IllegalArgumentException("StartSessionUseCase: Params can't be null!")
-//        val service = presentationManager.registry.serviceById[params.serviceId]?:
-        throw IllegalArgumentException("StartSessionUseCase: Requested service is not found")
-        return Completable.complete()
-//        return service.account.login(params.loginName, params.loginPassword)
-//                .flatMapCompletable {
-//                    presentationManager.selectContext(service.programId)
-//                    Completable.complete()
-//                }
+        return provider.accountService.login(params.loginName, params.loginPassword).ignoreElement()
     }
 
     class Params constructor (val loginName: String, val loginPassword: String, val serviceId: Long)
