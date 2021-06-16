b953404dfa67aec3c8559ffcde0c3edd43875941
Alex Sido <alesidos@gmail.com>
2021-06-16 11:21:09 +0300

    Equal versions of Kotlin as a Gradle Plugin and Kotlin as part of Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@
 buildscript {
 
     val kotlinVersion = "1.4.31"
+    val androidGradlePluginVersion = "4.2.1"
     val objectboxVersion = "2.7.0"
     val navigationVersion = "2.3.0"
 
@@ -14,7 +15,7 @@ buildscript {
         maven("https://dl.google.com/dl/android/maven2")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("com.android.tools.build:gradle:$androidGradlePluginVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("io.objectbox:objectbox-gradle-plugin:$objectboxVersion")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
