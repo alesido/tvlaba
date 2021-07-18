fa15ade9a9353bbd1f6b8dc4e56c70db64211c3f
Alex Sido <alesidos@gmail.com>
2021-07-18 11:47:13 +0300

    Resume playback or browsing on app restart (not in TV or VOD yet)

--- Domain/src/main/java/org/alsi/android/domain/context/interactor/TryResumeSessionUseCase.kt
+++ Domain/src/main/java/org/alsi/android/domain/context/interactor/TryResumeSessionUseCase.kt
@@ -4,18 +4,19 @@ import io.reactivex.Single
 import org.alsi.android.domain.context.model.PresentationManager
 import org.alsi.android.domain.context.model.ServicePresentationType
 import org.alsi.android.domain.context.model.SessionActivityType
+import org.alsi.android.domain.context.model.UserActivityRecord
 import org.alsi.android.domain.implementation.executor.PostExecutionThread
 import org.alsi.android.domain.implementation.interactor.SingleObservableUseCase
 import org.alsi.android.domain.streaming.model.ServiceProvider
 import org.alsi.android.domain.tv.repository.session.TvSessionRepository
+import org.joda.time.DateTimeUtils
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 /**
- *  Setup user session i.e.:
- *
- *  - require user to do login, if nobody logged in yet, or it was too long ago,
- *  - or make auto-login, if the last login record is quite old,
- *  - or even skip login, if the last login record is fresh enough.
+ *  Attempt to resume user session by skipping login or making auto-login if the
+ *  last session is quite recent. Require user to login, if nobody logged in yet,
+ *  or the last user activity was too long ago.
  */
 open class  TryResumeSessionUseCase @Inject constructor(
     private val presentationManager: PresentationManager,
@@ -29,12 +30,38 @@ open class  TryResumeSessionUseCase @Inject constructor(
         if (session !is TvSessionRepository)
             return Single.error(Throwable("The TV Directory Repository is N/A!"))
 
-        // TODO Add check for the latest browsing time
-        return session.play.latest().flatMap {
-            if (it.isEmpty()) {
+        return Single.zip(session.browse.mostRecent(), session.play.mostRecent(), { browse, play ->
+            Pair(browse, play)
+        }).flatMap {
+            val (browse, play) = it
+            if (browse.isEmpty() && play.isEmpty())
                 return@flatMap Single.just(SessionActivityType.LOGIN)
-            }
-            provider.accountService.resume(it.loginName, true).map { SessionActivityType.PLAYBACK_TV }
+            if (play.isEmpty())
+                return@flatMap resumeActivity(browse)
+            return@flatMap resumeActivity(
+                if (browse.timeStamp > play.timeStamp) browse else play)
         }
+    }
+
+    private fun resumeActivity(activity: UserActivityRecord): Single<SessionActivityType> {
+        val nowTimeStamp = DateTimeUtils.currentTimeMillis()
+        return when {
+            nowTimeStamp - activity.timeStamp < ELAPSED_TIME_LIMIT_FOR_AUTO_LOGIN -> {
+                provider.accountService.resume(activity.loginName, true)
+                    .map { SessionActivityType.PLAYBACK_TV }
+            }
+            nowTimeStamp - activity.timeStamp < ELAPSED_TIME_LIMIT_FOR_MANUAL_LOGIN -> {
+                provider.accountService.resume(activity.loginName, false)
+                    .map { SessionActivityType.PLAYBACK_TV }
+            }
+            else -> {
+                Single.just(SessionActivityType.LOGIN)
+            }
+        }
+    }
+
+    companion object {
+        val ELAPSED_TIME_LIMIT_FOR_MANUAL_LOGIN = TimeUnit.DAYS.toMillis(7)
+        val ELAPSED_TIME_LIMIT_FOR_AUTO_LOGIN = TimeUnit.DAYS.toMillis(1)
     }
 }
