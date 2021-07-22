5d0186ba1dae41517702cac8411e336e9adf1bec
Alex Sido <alesidos@gmail.com>
2021-07-22 14:01:04 +0300

    Resume playback or browsing: selecting initial context

--- Domain/src/main/java/org/alsi/android/domain/context/interactor/TryResumeSessionUseCase.kt
+++ Domain/src/main/java/org/alsi/android/domain/context/interactor/TryResumeSessionUseCase.kt
@@ -2,14 +2,12 @@ package org.alsi.android.domain.context.interactor
 
 import io.reactivex.Single
 import org.alsi.android.domain.context.model.PresentationManager
-import org.alsi.android.domain.context.model.ServicePresentationType
 import org.alsi.android.domain.context.model.SessionActivityType
 import org.alsi.android.domain.context.model.UserActivityRecord
 import org.alsi.android.domain.implementation.executor.PostExecutionThread
 import org.alsi.android.domain.implementation.interactor.SingleObservableUseCase
 import org.alsi.android.domain.streaming.model.ServiceProvider
 import org.alsi.android.domain.streaming.model.service.StreamingServiceRegistry
-import org.alsi.android.domain.tv.repository.session.TvSessionRepository
 import org.joda.time.DateTimeUtils
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -21,13 +19,14 @@ import javax.inject.Inject
  */
 open class  TryResumeSessionUseCase @Inject constructor(
     private val registry: StreamingServiceRegistry,
+    private val presentationManager: PresentationManager,
     private val provider: ServiceProvider,
     executionThread: PostExecutionThread)
     : SingleObservableUseCase<SessionActivityType, Nothing?>(executionThread)
 {
     override fun buildUseCaseObservable(params: Nothing?): Single<SessionActivityType> {
 
-        return Single.zip(registry.map { it.session.mostRecent(it.id) }) { zipArray ->
+        return Single.zip(registry.map { it.session.mostRecentActivity(it.id) }) { zipArray ->
             val nonEmpties = zipArray.filter { !(it as UserActivityRecord).isEmpty() }
             if (nonEmpties.isEmpty())
                 UserActivityRecord.empty()
@@ -45,12 +44,14 @@ open class  TryResumeSessionUseCase @Inject constructor(
         val nowTimeStamp = DateTimeUtils.currentTimeMillis()
         return when {
             nowTimeStamp - activity.timeStamp < ELAPSED_TIME_LIMIT_FOR_AUTO_LOGIN -> {
+                presentationManager.switchToContext(activity.serviceId)
                 provider.accountService.resume(activity.loginName, true)
-                    .map { SessionActivityType.PLAYBACK_TV }
+                    .map { activity.activityType }
             }
             nowTimeStamp - activity.timeStamp < ELAPSED_TIME_LIMIT_FOR_MANUAL_LOGIN -> {
+                presentationManager.switchToContext(activity.serviceId)
                 provider.accountService.resume(activity.loginName, false)
-                    .map { SessionActivityType.PLAYBACK_TV }
+                    .map { activity.activityType }
             }
             else -> {
                 Single.just(SessionActivityType.LOGIN)
